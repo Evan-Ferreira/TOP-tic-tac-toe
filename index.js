d638d4257e0a361f6f1860d48402d02cee584b9e
Evan Ferreira <evanjfer@gmail.com>
2024-12-28 00:48:49 -0500

    Add the final skeleton structure of Tic Tac Toe
    - Made a grid and centered it across the page
    - added 3x3 grid, each with x and y attributes

---
+++ index.js
@@ -0,0 +1,7 @@
+const displayMaster = (function () {
+    const updateDisplay = (x, y) => {
+        if (x < 0 || y < 0 || x > 2 || y > 2) {
+            console.log('Invalid input');
+        }
+    };
+})();
